0929311658742f65c51ba7f8d02084b2a1a72772
Noah Kim <dongwoo_333@hanmail.net>
2023-01-23 17:10:36 +1100

    32. Code Quiz! User Reservations

--- 04-base-concert-venue/__tests__/__mocks__/msw/handlers.ts
+++ 04-base-concert-venue/__tests__/__mocks__/msw/handlers.ts
@@ -1,6 +1,7 @@
 import { rest } from "msw";
 
 import { readFakeData } from "@/__tests__/__mocks__/fakeData";
+import { fakeUserReservations } from "@/__tests__/__mocks__/fakeData/userReservations";
 
 export const handlers = [
   rest.get("http://localhost:3000/api/shows/:showId", async (req, res, ctx) => {
@@ -11,4 +12,8 @@ export const handlers = [
     // index / showId = 1 has NO seats available
     return res(ctx.json({ show: fakeShows[Number(showId)] }));
   }),
+  rest.get(
+    "http://localhost:3000/api/users/:userId/reservations",
+    (req, res, ctx) => res(ctx.json({ userReservations: fakeUserReservations }))
+  ),
 ];
