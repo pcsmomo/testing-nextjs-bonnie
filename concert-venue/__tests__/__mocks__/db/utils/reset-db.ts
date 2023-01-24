98c3fa85cd442da051d985a38b0ba19f09bd5e43
Noah Kim <dongwoo_333@hanmail.net>
2023-01-24 21:06:33 +1100

    42. Create a Method to Reset the Test DB

---
+++ concert-venue/__tests__/__mocks__/db/utils/reset-db.ts
@@ -0,0 +1,23 @@
+import { readFakeData } from "@/__tests__/__mocks__/fakeData";
+import { filenames, writeJSONToFile } from "@/lib/db/db-utils";
+
+export const resetDB = async () => {
+  // failsafe against resetting production db!
+  const safeToReset = process.env.NODE_ENV === "test";
+  if (!safeToReset) {
+    // eslint-disable-next-line no-console
+    console.log("WARNING: DB reset attempted outside of test environment!");
+    return;
+  }
+
+  const { fakeShows, fakeBands, fakeUsers, fakeReservations } =
+    await readFakeData();
+
+  // overwrite data in files
+  await Promise.all([
+    writeJSONToFile(filenames.bands, fakeBands),
+    writeJSONToFile(filenames.shows, fakeShows),
+    writeJSONToFile(filenames.reservations, fakeReservations),
+    writeJSONToFile(filenames.users, fakeUsers),
+  ]);
+};
