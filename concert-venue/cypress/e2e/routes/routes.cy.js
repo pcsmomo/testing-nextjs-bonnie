5e5674a7c2f02a397088969cfa61f7743d741d5e
Noah Kim <dongwoo_333@hanmail.net>
2023-01-26 08:32:18 +1100

    54. Code Quiz! Static Route

--- concert-venue/cypress/e2e/routes/routes.cy.js
+++ concert-venue/cypress/e2e/routes/routes.cy.js
@@ -3,3 +3,11 @@ it("displays correct heading when navigating to shows route", () => {
   cy.findByRole("button", { name: /shows/i }).click();
   cy.findByRole("heading", { name: /upcoming shows/i }).should("exist");
 });
+
+it("displays correct heading when navigating to band route", () => {
+  cy.visit("/");
+  cy.findByRole("button", { name: /bands/i }).click();
+  cy.findByRole("heading", { name: /our illustrious performers/i }).should(
+    "exist"
+  );
+});
