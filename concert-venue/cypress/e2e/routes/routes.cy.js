617bf0c2765c81aa97dbc0df810b176c5f4dda35
Noah Kim <dongwoo_333@hanmail.net>
2023-01-26 09:06:53 +1100

    61. Run all Cypress and Jest Tests

--- concert-venue/cypress/e2e/routes/routes.cy.js
+++ concert-venue/cypress/e2e/routes/routes.cy.js
@@ -1,3 +1,8 @@
+import { generateNewBand } from "../../../__tests__/__mocks__/fakeData/newBand";
+import { generateRandomId } from "../../../lib/features/reservations/utils";
+// import { generateNewBand } from "@/__tests__/__mocks__/fakeData/newBand";
+// import { generateRandomId } from "@/lib/features/reservations/utils";
+
 it("displays correct heading when navigating to shows route", () => {
   cy.visit("/");
   cy.findByRole("button", { name: /shows/i }).click();
@@ -20,4 +25,11 @@ it("displays correct band name for band route that existed at build time", () =>
 it("displays error for band not in db", () => {
   cy.task("db:reset").visit("/bands/12345");
   cy.findByText(/Error: band not found/i).should("exist");
+});
+
+it("displays name for band that was not present at build time", () => {
+  const bandId = generateRandomId();
+  const newBand = generateNewBand(bandId);
+  cy.task("db:reset").task("addBand", newBand).visit(`/bands/${bandId}`);
+  cy.findByRole("heading", { name: /Avalanche of Cheese/i }).should("exist");
 });
